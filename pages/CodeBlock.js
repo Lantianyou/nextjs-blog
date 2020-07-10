7a9f49c2e3d89dd96d9ef075ad19611b2e82d959
Lantianyou <dtlantianyou@gmail.com>
2020-07-10 22:46:14 +0800

    code theme

--- pages/CodeBlock.js
+++ pages/CodeBlock.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 import { mdx } from '@mdx-js/react'
+import theme from "prism-react-renderer/themes/github";
+
 export default ({ children, className, live, render }) => {
     const language = className.replace(/language-/, '')
     if (live) {
@@ -29,7 +31,7 @@ export default ({ children, className, live, render }) => {
         )
     }
     return (
-        <Highlight {...defaultProps} code={children.trim()} language={language}>
+        <Highlight {...defaultProps} code={children.trim()} theme={theme} language={language}>
             {({ className, style, tokens, getLineProps, getTokenProps }) => (
                 <pre className={className} style={{ ...style, padding: '20px' }}>
                     {tokens.map((line, i) => (
