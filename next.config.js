af0605e3bf832527e2ec42a5156ea72929b19276
Lantianyou <dtlantianyou@gmail.com>
2020-07-10 21:47:31 +0800

    mdx fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const rehypePrism = require('@mapbox/rehype-prism')
+
 const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/,
 })
 
 module.exports = withMDX({
     pageExtensions: ['ts', 'tsx', 'mdx'],
+    options: {
+        rehypePlugins: [rehypePrism],
+    },
 })
