c46a40499012aab810e2d60ce46b0f05cc72a9ee
Lantianyou <dtlantianyou@gmail.com>
2020-07-18 21:23:36 +0800

    favicon

--- components/meta.tsx
+++ components/meta.tsx
@@ -34,9 +34,9 @@ export default function Meta() {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@dtlantianyou" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="180x180" href="../public/assets/favicon_io/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="../public/assets/favicon_io/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="../public/assets/favicon_io/favicon-16x16.png" />
-        <link rel="manifest" href="../public/assets/favicon_io/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon_io/favicon-16x16.png" />
+        <link rel="manifest" href="assets/favicon_io/site.webmanifest" />
     </Head>)
 }
