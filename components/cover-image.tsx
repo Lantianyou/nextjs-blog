a042dbd2caa7b1fdd60f791403a3075467b4d5b7
Lantianyou <dtlantianyou@gmail.com>
2020-07-19 08:35:29 +0800

    refactor cover-image“;

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -2,13 +2,16 @@ import Link from 'next/link'
 import cn from 'classname'
 
 export default function CoverImage({ title, src, slug }) {
+    if (!src) {
+        return <div></div>
+    }
     const image = (
         <div>
-            {src && (<img src={require(`../images/${src}`)}
+            <img src={require(`../images/${src}`)}
                 alt={`Cover Image for ${title}`}
                 className={cn('shadow-small', {
                     'hover:shadow-medium transition-shadow duration-200': slug,
-                })} />)}
+                })} />
         </div>
     )
     return (
