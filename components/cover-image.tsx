4e761d5ec094f59ad9bad6d0f2914b203c154345
Lantianyou <dtlantianyou@gmail.com>
2020-07-19 08:34:08 +0800

    delete post.tsx

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -3,13 +3,13 @@ import cn from 'classname'
 
 export default function CoverImage({ title, src, slug }) {
     const image = (
-        <>
-            <img src={require(`../images/${src}`)}
+        <div>
+            {src && (<img src={require(`../images/${src}`)}
                 alt={`Cover Image for ${title}`}
                 className={cn('shadow-small', {
                     'hover:shadow-medium transition-shadow duration-200': slug,
-                })} />
-        </>
+                })} />)}
+        </div>
     )
     return (
         <div className="-mx-5 sm:mx-0">
