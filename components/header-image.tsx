8b13f987f4b5e30f2f099bb36d0602548fba8501
Lantianyou <dtlantianyou@gmail.com>
2020-07-22 11:08:16 +0800

    state?

--- components/header-image.tsx
+++ components/header-image.tsx
@@ -1,50 +1,8 @@
-import { useState, useEffect } from 'react'
 import SiteBranding from 'components/header/site-branding'
 import SiteNav from 'components/header/site-nav'
 import ProgressBar from 'components/header/progress-bar'
 
 function HeaderImage() {
-
-  const [scrollY, setScrollY] = useState(0)
-  const [scrollPosition, setScrollPosition] = useState(0)
-
-  useEffect(() => {
-    window.addEventListener('scroll', onScroll)
-    return window.removeEventListener('scroll', onScroll)
-  }, [])
-
-  const onScroll = () => {
-    setScrollY(window.scrollY)
-    console.log('!!!')
-    console.log(window.scrollY)
-  }
-
-  // const caculateScrollDistance = () => {
-  //   const scrollTop = window.pageYOffset
-  //   console.log("caculateScrollDistance -> scrollTop", scrollTop)
-  //   const windowHeight = window.innerHeight;
-  //   console.log("caculateScrollDistance -> windowHeight", windowHeight)
-  //   const docHeight = getDocHeight();
-  //   console.log("caculateScrollDistance -> docHeight", docHeight)
-  //   const totalDocScrollLength = docHeight - windowHeight
-  //   const scrollPosition = Math.floor(scrollTop / totalDocScrollLength)
-  //   setScrollPosition(scrollPosition)
-  //   setScrollY(scrollTop)
-  //   console.log(scrollTop)
-  // }
-
-  // Now getting the document’s height is tricky and 
-  // the reason behind this is because various browsers have different ways 
-  // in which they interpret or calculate the height of a document.
-  // const getDocHeight = () => {
-  //   return Math.max(
-  //     document.body.scrollHeight, document.documentElement.scrollHeight,
-  //     document.body.offsetHeight, document.documentElement.offsetHeight,
-  //     document.body.clientHeight, document.documentElement.clientHeight
-  //   );
-  // }
-
-
 
   const opacity = Math.max(1 - scrollY / 547, 0)
 
@@ -59,7 +17,7 @@ function HeaderImage() {
       </div>
     </header>
     <SiteNav />
-    <ProgressBar progress={scrollPosition} />
+    <ProgressBar progress={75} />
   </>
   )
 }
--- pages/api/file.ts
+++ pages/api/file.ts
@@ -1,16 +1,39 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import firebase from 'lib/firebase'
-import 'firebase/firestore'
+import 'firebase/storage'
 
-const getFile = async () => {
-    const fireStore = firebase.firestore()
-    const snapshots = await fireStore.collection('posts').get()
-    snapshots.docs.forEach(doc => console.log(doc.id))
-    return snapshots.docs.length
+const getFile = async (fileName = 'resume.pdf') => {
+  const storage = firebase.storage()
+  const storageRef = storage.ref()
+  const fileRef = storageRef.child(fileName)
+
+  fileRef.getDownloadURL().then(function (url) {
+  }).catch(function (error) {
+
+    // A full list of error codes is available at
+    // https://firebase.google.com/docs/storage/web/handle-errors
+    switch (error.code) {
+      case 'storage/object-not-found':
+        // File doesn't exist
+        break;
+
+      case 'storage/unauthorized':
+        // User doesn't have permission to access the object
+        break;
+
+      case 'storage/canceled':
+        // User canceled the upload
+        break;
+      case 'storage/unknown':
+        // Unknown error occurred, inspect the server response
+        break;
+    }
+  })
+
 }
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-    const fileName = req.query
-    // getFile()
-    res.status(200).json({ text: getFile() })
+  const fileName = req.query
+  getFile()
+  res.status(200).json({ text: getFile() })
 }
