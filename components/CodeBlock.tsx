f1e8b06caba5178c7879ab15bb467d981701febc
Lantianyou <dtlantianyou@gmail.com>
2020-07-25 22:39:26 +0800

    to loading

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,30 +1,43 @@
-import Highlight, { defaultProps } from 'prism-react-renderer'
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ThemeContext } from 'styled-components';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Fade } from 'react-reveal'
+import Highlight, { defaultProps } from 'prism-react-renderer'
 import nightOwl from "prism-react-renderer/themes/nightOwl";
 import nightOwlLight from "prism-react-renderer/themes/nightOwlLight";
+import Message from 'components/message'
 
 
 const CodeBlock = ({ children, className }) => {
-    const language = className.replace(/language-/, '')
-    const themeContext = useContext(ThemeContext);
-    const codeTheme = themeContext['theme'] === 'dark' ? nightOwl : nightOwlLight
+  const language = className.replace(/language-/, '')
+  const themeContext = useContext(ThemeContext);
+  const codeTheme = themeContext['theme'] === 'dark' ? nightOwl : nightOwlLight
+  const text = children.toString()
+  const [copied, setCopied] = useState(false)
 
-    return (
-        <Highlight {...defaultProps} code={children} theme={codeTheme} language={language}>
-            {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <pre className={className} style={{ ...style, padding: '20px' }}>
-                    {tokens.map((line, i) => (
-                        <div key={i} {...getLineProps({ line, key: i })}>
-                            {line.map((token, key) => (
-                                <span key={key} {...getTokenProps({ token, key })} />
-                            ))}
-                        </div>
-                    ))}
-                </pre>
-            )}
-        </Highlight>
-    )
+  const copy = (<div className='text-center py-4'>
+    < CopyToClipboard text={text} onCopy={() => setCopied(true)} >
+      <button>复制</button>
+    </ CopyToClipboard></div>)
+
+  return (
+    <div>
+      {copied ? <Fade top distance='10px'><Message message='已复制' newMessage={false} /> </Fade> : copy}
+      <Highlight {...defaultProps} code={children} theme={codeTheme} language={language}>
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={{ ...style, padding: '20px' }}>
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    </div>
+  )
 }
 
 export default CodeBlock
--- components/non-markdown.tsx
+++ components/non-markdown.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import ProgressBar from 'components/post/progress-bar'
 import Container from 'components/container'
 import PostHeader from 'components/post/post-header'
@@ -5,7 +6,15 @@ import PostBody from 'components/post/post-body'
 import SiteNav from './header/site-nav'
 
 export default function MarkDownContent({ title, date, children }) {
-  return (
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(false)
+  }, [])
+
+
+
+  return loading ? <div>isloading</div> :
     <>
       <SiteNav />
       <ProgressBar />
@@ -16,5 +25,4 @@ export default function MarkDownContent({ title, date, children }) {
         </PostBody>
       </Container>
     </>
-  )
 }
