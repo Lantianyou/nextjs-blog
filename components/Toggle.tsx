ab37da1a0507f0f5774a522e25ecc43214d38222
Lantianyou <dtlantianyou@gmail.com>
2020-07-26 15:11:16 +0800

    rebass

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,4 +1,6 @@
-import {Moon, Sun} from 'react-feather'
+import cn from 'classnames'
+import { Moon, Sun } from 'react-feather'
+import { Button } from 'rebass'
 import { useDispatch, connect } from 'react-redux'
 import { TOGGLE_DARKTHEME } from 'lib/actions'
 
@@ -6,50 +8,12 @@ const Toggle = ({ darkThemeEnabled }) => {
   const dispatch = useDispatch()
 
   return (
-    <button aria-label="Toggle theme" onClick={() => dispatch({ type: TOGGLE_DARKTHEME })} className='w-8'>
-      <style jsx>{`
-            --bottom-color: #999;
-            --right-color: #ddd;
-            --top-color: #ccc;
-            button {
-              background: var(--top-color);
-              box-shadow:
-                1px 0   0 var(--right-color),
-                1px 1px 0 var(--bottom-color),
-                2px 1px 0 var(--right-color),
-                2px 2px 0 var(--bottom-color),
-                3px 2px 0 var(--right-color),
-                3px 3px 0 var(--bottom-color),
-                4px 3px 0 var(--right-color),
-                4px 4px 0 var(--bottom-color),
-                5px 4px 0 var(--right-color),
-                5px 5px 0 var(--bottom-color),
-                6px 5px 0 var(--right-color),
-                6px 6px 0 var(--bottom-color),
-                7px 6px 0 var(--right-color),
-                7px 7px 0 var(--bottom-color),
-                8px 7px 0 var(--right-color),
-                8px 8px 0 var(--bottom-color);
-              transition: 
-                box-shadow 0.2s,
-                transform 0.15s;
-              }
-              button:hover {
-                outline: 0;
-                box-shadow:
-                  1px 0   0 var(--right-color),
-                  1px 1px 0 var(--bottom-color),
-                  2px 1px 0 var(--right-color),
-                  2px 2px 0 var(--bottom-color),
-                  3px 2px 0 var(--right-color),
-                  3px 3px 0 var(--bottom-color),
-                  4px 3px 0 var(--right-color),
-                  4px 4px 0 var(--bottom-color);
-                  transform: translate(3px, 3px);
-                }
-            `}</style>
-      {darkThemeEnabled ? <Sun size={30} /> : <Moon size={30} />}
-    </button>
+    <div className={cn({ 'bg-gray-600': darkThemeEnabled }, { 'bg-black': !darkThemeEnabled })}>
+      <Button onClick={() => dispatch({ type: TOGGLE_DARKTHEME })}>
+        {darkThemeEnabled ? <Sun size={30} /> : <Moon size={30} />}
+      </Button>
+
+    </div>
   );
 }
 
@@ -59,3 +23,49 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps)(Toggle)
+
+
+//   < button aria - label="Toggle theme" onClick = {() => dispatch({ type: TOGGLE_DARKTHEME })} className = 'w-8' >
+//     <style jsx>{`
+//             --bottom-color: #999;
+//             --right-color: #ddd;
+//             --top-color: #ccc;
+//             button {
+//               background: var(--top-color);
+//               box-shadow:
+//                 1px 0   0 var(--right-color),
+//                 1px 1px 0 var(--bottom-color),
+//                 2px 1px 0 var(--right-color),
+//                 2px 2px 0 var(--bottom-color),
+//                 3px 2px 0 var(--right-color),
+//                 3px 3px 0 var(--bottom-color),
+//                 4px 3px 0 var(--right-color),
+//                 4px 4px 0 var(--bottom-color),
+//                 5px 4px 0 var(--right-color),
+//                 5px 5px 0 var(--bottom-color),
+//                 6px 5px 0 var(--right-color),
+//                 6px 6px 0 var(--bottom-color),
+//                 7px 6px 0 var(--right-color),
+//                 7px 7px 0 var(--bottom-color),
+//                 8px 7px 0 var(--right-color),
+//                 8px 8px 0 var(--bottom-color);
+//               transition: 
+//                 box-shadow 0.2s,
+//                 transform 0.15s;
+//               }
+//               button:hover {
+//                 outline: 0;
+//                 box-shadow:
+//                   1px 0   0 var(--right-color),
+//                   1px 1px 0 var(--bottom-color),
+//                   2px 1px 0 var(--right-color),
+//                   2px 2px 0 var(--bottom-color),
+//                   3px 2px 0 var(--right-color),
+//                   3px 3px 0 var(--bottom-color),
+//                   4px 3px 0 var(--right-color),
+//                   4px 4px 0 var(--bottom-color);
+//                   transform: translate(3px, 3px);
+//                 }
+//             `}</style>
+// { darkThemeEnabled ? <Sun size={30} /> : <Moon size={30} /> }
+//       </button >
