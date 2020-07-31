926eebc7563248d80537e711f5917451830d93dd
Lantianyou <dtlantianyou@gmail.com>
2020-07-31 23:20:27 +0800

    toggle button

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -7,7 +7,7 @@ const Toggle = ({ darkThemeEnabled }) => {
   const dispatch = useDispatch()
 
   return (
-    <button aria-label='切换暗色模式' onClick={() => dispatch({ type: TOGGLE_DARKTHEME })} style={{ height: '30px' }} className={cn('my-auto', { 'bg-gray-600': darkThemeEnabled }, { 'bg-black': !darkThemeEnabled })} >
+    <button aria-label='切换暗色模式' onClick={() => dispatch({ type: TOGGLE_DARKTHEME })} style={{ height: '30px' }} className={cn('my-auto', { 'bg-gray-600': darkThemeEnabled }, { 'bg-gray-100': !darkThemeEnabled })} >
       {darkThemeEnabled ? <Sun size={30} /> : <Moon size={30} />}
     </button>
   );
