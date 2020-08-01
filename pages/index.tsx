80af4dbe2dbf86efe44dfbaaae18af922393740d
Lantianyou <dtlantianyou@gmail.com>
2020-08-01 22:08:14 +0800

    index refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Layout from 'components/layout'
 import { siteTitle } from 'components/meta'
 import Container from 'components/container'
 import SiteNav from 'components/header/site-nav'
+import PostDate from 'components/post/post-date'
 import { getPostsMetadata } from 'lib/getPosts'
 
 
@@ -13,6 +14,50 @@ export default function Home({ postsMetadata }) {
     // const morePosts = postsMetadata.slice(1)
     return (
         <Layout preview={false}>
+            <style jsx>{`
+                ul li {
+                    padding: 10px 15px;
+                }
+                ul li span {
+                    color: #5b5b5b;
+                    display: block;
+                    font-size: 13px;
+                }
+                ul li a {
+                    font-weight: bold;
+                    color: var(--link-color);
+                    text-decoration: none;
+                }
+                @media (any-hover: hover) {
+                    ul li a:hover {
+                    background: #eee;
+                    }
+                    ul li a:active {
+                    background: #ccc;
+                    }
+                }
+                @media (min-width: 500px) {
+                    ul {
+                    padding: 20px 0;
+                    max-width: 42rem;
+                    margin: auto;
+                    }
+                    ul li {
+                    padding-left: 0;
+                    }
+                    ul li a {
+                    padding: 10px 15px;
+                    transition: 150ms background-color ease-in;
+                    }
+                    ul li span {
+                    display: inline-block;
+                    width: 160px;
+                    padding-right: 10px;
+                    text-align: right;
+                    font-size: inherit;
+                    }
+                }
+            `}</style>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
@@ -22,6 +67,7 @@ export default function Home({ postsMetadata }) {
                 <div className="max-w-2xl mx-auto">
                     <ul>
                         {postsMetadata.map(postMetadata => <li>
+                            <span><PostDate dateString={postMetadata.date} /></span>
                             <Link href={`posts/${postMetadata.slug}`}>
                                 {postMetadata.title}
                             </Link>
