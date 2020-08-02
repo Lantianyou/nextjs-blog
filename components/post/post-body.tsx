5c9c65c3235645910fa8f0dd5f8b8088f77f5efe
Lantianyou <dtlantianyou@gmail.com>
2020-08-02 17:51:07 +0800

    markdown css static

--- components/post/post-body.tsx
+++ components/post/post-body.tsx
@@ -1,9 +1,6 @@
-import cn from 'classnames'
-import styles from 'styles/github-markdown.module.css'
-
 export default function PostBody({ children }) {
     return (
-        <article id="article-top" className={cn('max-w-2xl', 'mx-auto', 'markdown-body', styles['markdown-body'])}>
+        <article id="article-top" className='max-w-2xl mx-auto'>
             {children}
         </article >
     )
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,14 +1,16 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
 import Head from 'next/head'
+import cn from 'classnames'
 import Post from 'components/post/post'
+import styles from 'styles/github-markdown.module.css'
 import { getPostsSlug, getPostAndMetadata } from 'lib/getPosts'
 
 const Blog = ({ htmlString, data, slug }) => {
   const { author, date, title, cover, excerpt } = data
-  const withMath = slug === 'pvalue-misconception'
+  const useMath = slug === 'pvalue-misconception'
   return (
     <>
-      {withMath && <Head>
+      {useMath && <Head>
         {/* <link rel="stylesheet" href="https://latex.now.sh/style.css" /> */}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.11.0/dist/katex.min.css" integrity="sha384-BdGj8xC2eZkQaxoQ8nSLefg4AV4/AwB3Fj+8SUSo7pnKP6Eoy18liIKTPn9oBYNG" crossOrigin="anonymous" />
       </Head>}
@@ -23,7 +25,7 @@ const Blog = ({ htmlString, data, slug }) => {
           to   { background: transparent; }
         }
         `}</style>
-        <div dangerouslySetInnerHTML={{ __html: htmlString }} />
+        <div dangerouslySetInnerHTML={{ __html: htmlString }} className={cn('markdown-body', styles['markdown-body'])} />
       </Post>
     </>
   )
