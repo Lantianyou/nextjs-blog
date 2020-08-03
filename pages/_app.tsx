5408356782005e952d363dd05b5a09a3f744037a
Lantianyou <dtlantianyou@gmail.com>
2020-08-03 19:23:42 +0800

    typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,16 @@ import 'styles/global.css'
 import DarkThemeProvider from 'components/DarkThemeProvider'
 import { AppProps } from 'next/app'
 import { MDXProvider } from '@mdx-js/react'
-import CodeBlock from 'components/CodeBlock'
+// import CodeBlock from 'components/CodeBlock'
 import Layout from 'components/layout'
 import SiteNav from 'components/header/site-nav'
 import ThemeContext from 'components/theme-context'
 
 
-const mdComponents = {
-  pre: props => <div {...props} />,
-  code: CodeBlock
-}
+// const mdComponents = {
+//   pre: props => <div {...props} />,
+//   code: CodeBlock
+// }
 
 export default function App({ Component, pageProps }: AppProps) {
   const theme = useState(false)
