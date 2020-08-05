39358f79461cfb29706c8cf652e36a8fc952a8bc
Lantianyou <dtlantianyou@gmail.com>
2020-08-05 16:06:15 +0800

    theme folder

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,34 +1,50 @@
-import { useContext, useState } from 'react';
+import { useContext, useState } from "react";
 // import { ThemeContext } from 'styled-components';
-import CopyToClipboard from 'react-copy-to-clipboard';
+import CopyToClipboard from "react-copy-to-clipboard";
 // import { Fade } from 'react-reveal'
-import Highlight, { defaultProps, Language } from 'prism-react-renderer'
+import Highlight, { defaultProps, Language } from "prism-react-renderer";
 import nightOwl from "prism-react-renderer/themes/nightOwl";
 import nightOwlLight from "prism-react-renderer/themes/nightOwlLight";
-import Message from 'components/message'
-import ThemeContext from 'components/theme-context'
+import Message from "components/message";
+import ThemeContext from "components/theme/theme-context";
 
-const CodeBlock = ({ children, className }: { children: string, className: string }) => {
-  const language = className.replace(/language-/, '') as Language
+const CodeBlock = ({
+  children,
+  className,
+}: {
+  children: string;
+  className: string;
+}) => {
+  const language = className.replace(/language-/, "") as Language;
   const [darkThemeEnabled] = useContext(ThemeContext);
-  const codeTheme = darkThemeEnabled ? nightOwl : nightOwlLight
-  const text = children.toString()
-  const [copied, setCopied] = useState(false)
+  const codeTheme = darkThemeEnabled ? nightOwl : nightOwlLight;
+  const text = children.toString();
+  const [copied, setCopied] = useState(false);
 
-  const copy = (<div className='text-center py-4'>
-    < CopyToClipboard text={text} onCopy={() => setCopied(true)} >
-      <button aria-label='复制代码块'>复制</button>
-    </ CopyToClipboard></div>)
+  const copy = (
+    <div className="text-center py-4">
+      <CopyToClipboard text={text} onCopy={() => setCopied(true)}>
+        <button aria-label="复制代码块">复制</button>
+      </CopyToClipboard>
+    </div>
+  );
 
   return (
     <div>
-      {copied ?
+      {copied ? (
         // <Fade top distance='10px'>
-        <Message message='已复制' newMessage={false} />
-        : copy}
-      <Highlight {...defaultProps} code={children} theme={codeTheme} language={language}>
+        <Message message="已复制" newMessage={false} />
+      ) : (
+        copy
+      )}
+      <Highlight
+        {...defaultProps}
+        code={children}
+        theme={codeTheme}
+        language={language}
+      >
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={{ ...style, padding: '20px' }}>
+          <pre className={className} style={{ ...style, padding: "20px" }}>
             {tokens.map((line, i) => (
               <div key={i} {...getLineProps({ line, key: i })}>
                 {line.map((token, key) => (
@@ -40,7 +56,7 @@ const CodeBlock = ({ children, className }: { children: string, className: strin
         )}
       </Highlight>
     </div>
-  )
-}
+  );
+};
 
-export default CodeBlock
+export default CodeBlock;
--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,23 +1,22 @@
-import { useContext } from 'react'
-import cn from 'classnames'
-import { Moon, Sun } from 'react-feather'
-import ThemeContext from 'components/theme-context'
+import { useContext } from "react";
+import cn from "classnames";
+import { Moon, Sun } from "react-feather";
+import ThemeContext from "components/theme/theme-context";
 
 const Toggle = () => {
-
-  const [darkThemeEnabled, setDarkThemeEnabled] = useContext(ThemeContext)
-  const toggleTheme = () => setDarkThemeEnabled(!darkThemeEnabled)
+  const [darkThemeEnabled, setDarkThemeEnabled] = useContext(ThemeContext);
+  const toggleTheme = () => setDarkThemeEnabled(!darkThemeEnabled);
 
   return (
     <button
-      aria-label='切换暗色模式'
+      aria-label="切换暗色模式"
       onClick={toggleTheme}
-      style={{ height: '30px' }}
-      className={cn('my-auto', { 'bg-gray-100': !darkThemeEnabled })} >
+      style={{ height: "30px" }}
+      className={cn("my-auto", { "bg-gray-100": !darkThemeEnabled })}
+    >
       {darkThemeEnabled ? <Sun size={30} /> : <Moon size={30} />}
     </button>
   );
-}
+};
 
-
-export default Toggle
+export default Toggle;
