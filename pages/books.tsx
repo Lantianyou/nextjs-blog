b0241c0bdcd34a3ae37f274e401c12d34588a397
Lantianyou <dtlantianyou@gmail.com>
2020-08-09 09:56:14 +0800

    books I like

--- pages/books.tsx
+++ pages/books.tsx
@@ -1,3 +1,15 @@
+import ContentContainer from "components/content-container";
+
 export default function () {
-  return <div>books I like</div>;
+  return (
+    <ContentContainer>
+      <h2>Books I Like</h2>
+      <div className="grid">
+        <h3>从零到一</h3>
+        <h3>投资中最重要的事</h3>
+        <h3>周期</h3>
+        <h3>反脆弱</h3>
+      </div>
+    </ContentContainer>
+  );
 }
